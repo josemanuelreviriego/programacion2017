fcd3b108be7cd574f3ca15ca396b122848e73a03
josemanuelreviriego <josemanuelreviriego.ciclos@gmail.com>
2017-05-30 14:26:48 +0200

    Corrección de errores

--- PunFunciones/PunMulti.cpp
+++ PunFunciones/PunMulti.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MUL operadores[0]
+#define RES operadores[1]
+
+
 int mul(int op1, int op2){return op1 * op2;}
 int resto(int op1, int op2){return op1 % op2;}
 
@@ -9,20 +13,19 @@ int main(int argc, char const *argv[])
 	char user;
 	int a = 12, b = 5;
 	int (*p)(int, int);
-
-        p = resto;
+        char operadores[]={'*', '/'};
 
 	printf("multiplica (s/n): ");
 	scanf(" %c", &user);
 
-	if (user == 's')
+	if (user == 's'){
             p = mul;
+        printf("%i %c %i = %i \n", a, MUL, b, p(a,b));
+}
+        else{
+            p = resto;
 
-		printf("%i * %i = %i \n", a, b, p(a,b));
-
-            p = &resto;
-
-	printf("%i / %i = %i \n", a, b, p(a,b));
-
+	printf("%i %c %i = %i \n", a, RES, b, p(a,b));
+        }
 	return EXIT_SUCCESS;
 }
